1a860688a81c219d82f88bd95b1796d9514056b0
muratcan23 <muratal0606@gmail.com>
2024-05-06 02:02:27 +0300

    add mongo to Node.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is listening to PORT: ${PORT}`);
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN project");
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to PORT: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
