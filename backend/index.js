3af5033384d1f94101d60ecded0c5ced2e54f2be
muratcan23 <muratal0606@gmail.com>
2024-05-08 18:58:51 +0300

    get book from db

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,21 @@ app.post("/books", async (request, response) => {
   }
 });
 
+//get all books
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
