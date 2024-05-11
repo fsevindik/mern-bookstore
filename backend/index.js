553b4f72a05bb74033d7e366ee6f58aece3a6f03
muratcan23 <muratal0606@gmail.com>
2024-05-11 04:09:21 +0300

    add CORS policy in Node.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
@@ -7,11 +8,25 @@ const app = express();
 
 app.use(express.json());
 
+//Middleware for handling CORS Policy
+//option1
+// app.use(cors())
+
+//option2 Allow Custom Origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to MERN project");
 });
 
+//This part is middleware for parsing request body
 app.use("/books", booksRoute);
 
 mongoose
