e5fc428a402ed2f53dddd551ff244b09f6b20026
muratcan23 <muratal0606@gmail.com>
2024-06-07 14:22:53 +0300

    add header

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { AiOutlineBook, AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-400 text-white p-4 shadow-md">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="text-2xl font-bold italic">
+          <Link to="/">Books are the only poison with an antidote</Link>
+        </div>
+        <nav className="ml-auto flex space-x-4">
+          <Link to="/" className="flex items-center hover:text-gray-300">
+            <AiOutlineHome className="mr-1" />
+            Home
+          </Link>
+          <Link to="/books" className="flex items-center hover:text-gray-300">
+            <AiOutlineBook className="mr-1" />
+            Books
+          </Link>
+          <Link to="/profile" className="flex items-center hover:text-gray-300">
+            <AiOutlineUser className="mr-1" />
+            Profile
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
