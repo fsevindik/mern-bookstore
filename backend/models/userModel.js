9cc05f6aa886f95384e918931a57367c445d4cd9
muratcan23 <muratal0606@gmail.com>
2024-06-26 22:56:56 +0300

    hasssshhhh  userPasswords

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -10,8 +11,23 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// Pre-save hook to hash the password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare entered password with stored hashed password
 userSchema.methods.matchPassword = async function (enteredPassword) {
-  return enteredPassword === this.password;
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);
