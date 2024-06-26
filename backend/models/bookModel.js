5c9fc2ebe98adfe5dd812fac126b91f4ad96cf0d
muratcan23 <muratal0606@gmail.com>
2024-06-26 12:51:16 +0300

    upgrade bookModel for comment reactions

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,5 +1,35 @@
 import mongoose from "mongoose";
 
+// Reaction schema
+const reactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "userId", // ref
+    required: false,
+  },
+  type: {
+    type: String,
+    enum: ["like", "heart", "smile", "surprise"],
+    required: false,
+  },
+});
+
+const commentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  reactions: [reactionSchema],
+});
+
 const bookSchema = new mongoose.Schema(
   {
     title: {
@@ -22,26 +52,11 @@ const bookSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    likes: {
+    rate: {
       type: Number,
       default: 0,
     },
-    comments: [
-      {
-        text: {
-          type: String,
-          required: true,
-        },
-        userName: {
-          type: String,
-          required: true,
-        },
-        createdAt: {
-          type: Date,
-          default: Date.now,
-        },
-      },
-    ],
+    comments: [commentSchema],
   },
   { timestamps: true }
 );
