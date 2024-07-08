49e08a47172f25b1077d18191b7487ad7262b621
muratcan23 <muratal0606@gmail.com>
2024-07-08 20:14:10 +0300

    fix import name

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import MessageButton from "../components/MessageButton";
+import MessageButton from "../components/MessageButton.jsx";
 
 const Welcome = () => {
   const userName = localStorage.getItem("UserName") || "Guest";
