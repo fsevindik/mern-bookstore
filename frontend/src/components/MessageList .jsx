ac61bf065ae9b1a1f43a80fc2e175c1a464b259d
muratcan23 <muratal0606@gmail.com>
2024-07-09 13:34:14 +0300

    improve admins ui

--- frontend/src/components/MessageList .jsx
+++ frontend/src/components/MessageList .jsx
@@ -62,18 +62,26 @@ const MessageList = () => {
             key={message._id}
             className="bg-slate-600 rounded-lg shadow-md mb-4"
           >
-            <div className="flex flex-col md:flex-row items-start p-4 border-2 border-gray-300">
-              <div className="text-black flex-1 mb-4 md:mb-0 md:mr-4">
-                <p className="bg-yellow-500 p-2 rounded mb-2 border-2 border-black">
+            <div
+              className={`flex ${
+                isSmallScreen ? "flex-col" : "flex-row"
+              } items-start p-4 border-2 border-gray-300`}
+            >
+              <div
+                className={`text-black ${
+                  isSmallScreen ? "mb-4" : "mr-4"
+                } md:mb-0 md:mr-4 flex-1`}
+              >
+                <p className="bg-slate-300 p-2 rounded mb-2 border-2 border-black">
                   <strong>🛂</strong>{" "}
                   <span className={isSmallScreen ? "truncate-id" : ""}>
                     {truncateID(message.sender, 10)}
                   </span>
                 </p>
-                <p className="bg-yellow-500 p-2 rounded mb-2 border-2 border-black">
+                <p className="bg-slate-300 p-2 rounded mb-2 border-2 border-black">
                   <strong>👤</strong> {message.recipient}
                 </p>
-                <p className="bg-slate-300 p-2 rounded mb-2 border-2 border-black font-serif font-bold">
+                <p className="bg-yellow-600 p-2 rounded mb-2 border-2 border-black font-serif font-bold">
                   <strong>💬</strong> {message.content}
                 </p>
                 <p className="bg-green-300 p-2 rounded border-2 border-black">
@@ -81,15 +89,19 @@ const MessageList = () => {
                   {new Date(message.sentAt).toLocaleString()}
                 </p>
               </div>
-              <div className="flex items-center justify-end mt-2 md:mt-0">
-                <button
-                  onClick={() => deleteMessage(message._id)}
-                  className="flex items-center bg-red-600 hover:bg-black text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
-                >
-                  <FaExclamationTriangle className="mr-2" />
-                  Delete
-                </button>
-              </div>
+            </div>
+            <div
+              className={`flex items-center justify-${
+                isSmallScreen ? "center" : "end"
+              } mt-2`}
+            >
+              <button
+                onClick={() => deleteMessage(message._id)}
+                className="flex items-center bg-red-600 hover:bg-black text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
+              >
+                <FaExclamationTriangle className="mr-2" />
+                Delete
+              </button>
             </div>
           </li>
         ))}
