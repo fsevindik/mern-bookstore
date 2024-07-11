b20a91ad09729c5fab42948d201f7c5dc37dfa05
muratcan23 <muratal0606@gmail.com>
2024-07-11 18:01:10 +0300

    improve style

--- frontend/src/components/allBooks/Trends.jsx
+++ frontend/src/components/allBooks/Trends.jsx
@@ -61,28 +61,28 @@ const Trends = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           {books.map((book) => (
             <Link key={book._id} to={`/books/details/${book._id}`}>
-              <div className="bg-yellow-500 p-4 rounded-lg shadow-md flex flex-col cursor-pointer">
-                <div className="relative pb-[95%] overflow-hidden rounded-lg">
+              <div className="bg-yellow-500 p-4 rounded-lg shadow-md flex flex-col h-[400px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105">
+                <div className="relative flex-grow overflow-hidden rounded-lg">
                   <img
                     src={book.imageA}
                     alt={book.title}
                     className="absolute top-0 left-0 w-full h-full object-cover rounded-lg"
                   />
                 </div>
-                <div className="mt-4 flex flex-col justify-between flex-grow">
+                <div className="mt-4 flex flex-col justify-between">
                   <div>
-                    <p className="text-blue-600 font-bold text-center md:text-md text-sm mb-1">
+                    <p className="text-blue-600 font-bold text-center md:text-md sm:text-sm mb-1">
                       <span className="text-white font-serif">
                         {truncateText(book.author, 20)}
                       </span>
                     </p>
-                    <h3 className="font-semibold text-center md:text-md">
+                    <h3 className="font-semibold text-center md:text-md sm:text-sm h-10 overflow-hidden">
                       {truncateText(book.title, 30)}
                     </h3>
                   </div>
                   <div className="flex items-center justify-center mt-2">
                     <FaStar className="text-blue-500 mr-1 text-sm" />
-                    <span className="text-white text-xs font-bold">
+                    <span className="text-white md:text-md sm:text-xs font-bold">
                       {book.averageRating
                         ? book.averageRating.toFixed(1)
                         : "N/A"}
