bef0a6943c14a292ee5b617655ad1ba3260e7912
muratcan23 <muratal0606@gmail.com>
2024-07-13 20:34:01 +0300

    correct import path

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import MessageList from "../components/MessageList";
+import MessageList from "../components/MessageList ";
 
 const Admin = () => {
   const [showMessages, setShowMessages] = useState(false);
