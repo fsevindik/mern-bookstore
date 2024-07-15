5152ede75f0674b2bb847f35913c5348b5f8a0b0
muratcan23 <muratal0606@gmail.com>
2024-07-15 17:16:37 +0300

    improve style

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useSnackbar } from "notistack";
 import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 import { API_URL } from "../../config";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
@@ -23,21 +24,22 @@ const DeleteBook = () => {
       })
       .catch((error) => {
         setLoading(false);
-        enqueueSnackbar("Error", { variant: "error" });
+        toast.error("Error", { variant: "error" });
         console.log(error);
       });
   };
 
   return (
-    <div className="p-4 bg-[#F5F5DC] min-h-screen flex-grow">
+    <div className="p-4 bg-[#F5F5DC] min-h-screen flex flex-col justify-center items-center">
       <BackButton />
       <h1 className="text-3xl my-4 text-center font-bold">Delete This Book</h1>
       {loading ? <Spinner /> : ""}
-      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
-        <h3 className="text-2xl">Are You Sure You want to delete this book?</h3>
-
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-full max-w-md p-8 mx-auto">
+        <h3 className="text-2xl text-center">
+          Are You Sure You want to delete this book?
+        </h3>
         <button
-          className="p-4 bg-red-600 text-white m-8 w-full hover:bg-red-900"
+          className="p-4 bg-red-600 text-white mt-8 w-full hover:bg-red-900"
           onClick={handleDeleteBook}
         >
           Yes, Delete it
